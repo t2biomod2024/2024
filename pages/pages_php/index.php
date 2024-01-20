1dd07c62cc24a6dff48b91e842a9fd86a929519d
Kakeru Matsuzaki <kakerudoraemon@gmail.com>
2024-01-21 02:27:37 +0900

    copy from kakeru-M's rep

---
+++ pages/pages_php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TeamTokyoTech / Home</title>
+        <link rel="shortcut icon" href="../../pics/EnemyBlack.png">
+        <link rel="stylesheet" href="../../css/reset.css">
+        <link rel="stylesheet" href="../../css/template.css">
+        <link rel="stylesheet" href="../../css/index.css">
+    </head>
+    <body>
+        <header>
+            <?php include "../../page_parts/header.html" ?>
+        </header>
+        <main>
+            <?php include "../pages_html/index.html" ?>
+        </main>
+        <footer>
+            <?php include "../../page_parts/footer.html" ?>
+        </footer>
+    </body>
+</html>
