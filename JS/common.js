8efb05b63178a45f1f2a2bc7f98f76421d30e87a
Ryusei334 <145414234+Ryusei334@users.noreply.github.com>
2024-09-14 02:39:00 +0900

    douki

--- JS/common.js
+++ JS/common.js
@@ -1,10 +1,28 @@
 // commonlize
-fetch("https://t2biomod2024.github.io/2024/page_parts/header.html")
-    .then((response) => response.text())
-    .then((data) => document.querySelector("#container").insertAdjacentHTML('afterbegin', data));
-fetch("https://t2biomod2024.github.io/2024/page_parts/footer.html")
-    .then((response) => response.text())
-    .then((data) => document.querySelector("#container").insertAdjacentHTML('beforeend', data));
-fetch("https://t2biomod2024.github.io/2024/page_parts/button.html")
-    .then((response) => response.text())
-    .then((data) => document.querySelector("#container").insertAdjacentHTML('afterend', data));
+//fetch("https://t2biomod2024.github.io/2024/page_parts/header.html")
+//    .then((response) => response.text())
+//    .then((data) => document.querySelector("#container").insertAdjacentHTML('afterbegin', data));
+//fetch("https://t2biomod2024.github.io/2024/page_parts/footer.html")
+//    .then((response) => response.text())
+//    .then((data) => document.querySelector("#container").insertAdjacentHTML('beforeend', data));
+//fetch("https://t2biomod2024.github.io/2024/page_parts/button.html")
+//    .then((response) => response.text())
+//    .then((data) => document.querySelector("#container").insertAdjacentHTML('afterend', data));
+
+    function fetchAndInsertSync(url, position) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, false); // 第3引数にfalseを設定すると同期リクエストになります
+        xhr.send();
+    
+        if (xhr.status === 200) {
+            document.querySelector("#container").insertAdjacentHTML(position, xhr.responseText);
+        } else {
+            console.error('Error fetching:', xhr.statusText);
+        }
+    }
+    
+    // 使用例
+    fetchAndInsertSync("https://t2biomod2024.github.io/2024/page_parts/header.html", 'afterbegin');
+    fetchAndInsertSync("https://t2biomod2024.github.io/2024/page_parts/footer.html", 'beforeend');
+    fetchAndInsertSync("https://t2biomod2024.github.io/2024/page_parts/button.html", 'afterend');
+    
