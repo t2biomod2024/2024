85b0e37733644042771f551b966640eb2a2bc28d
Ryusei334 <145414234+Ryusei334@users.noreply.github.com>
2024-09-14 01:28:50 +0900

    header

--- JS/common.js
+++ JS/common.js
@@ -2,7 +2,7 @@
 fetch("https://t2biomod2024.github.io/2024/page_parts/button.html")
     .then((response) => response.text())
     .then((data) => document.querySelector("body").insertAdjacentHTML('beforeend', data));
-fetch("https://t2biomod2024.github.io/2024/page_parts/footer.html")
+fetch("https://t2biomod2024.github.io/2024/page_parts/header.html")
     .then((response) => response.text())
     .then((data) => document.querySelector("#container").insertAdjacentHTML('afterbegin', data));
 fetch("https://t2biomod2024.github.io/2024/page_parts/footer.html")
