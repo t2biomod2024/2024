9af4342704387a4543d9c035820c6c4d32f4be57
Ryusei334 <145414234+Ryusei334@users.noreply.github.com>
2024-09-14 00:52:57 +0900

    EE

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,20 @@
 // commonlize
-document.addEventListener('DOMContentLoaded', function() {
-    fetch("https://t2biomod2024.github.io/2024/page_parts/header.html")
-        .then((response) => response.text())
-        .then((data) => document.querySelector("#container").insertAdjacentHTML('afterbegin', data));
-    fetch("https://t2biomod2024.github.io/2024/page_parts/footer.html")
-        .then((response) => response.text())
-        .then((data) => document.querySelector("#container").insertAdjacentHTML('beforeend', data));
-    fetch("https://t2biomod2024.github.io/2024/page_parts/button.html")
-        .then((response) => response.text())
-        .then((data) => document.querySelector("body").insertAdjacentHTML('beforeend', data));
+document.addEventListener('DOMContentLoaded', async function() {
+    try {
+        const headerResponse = await fetch("https://t2biomod2024.github.io/2024/page_parts/header.html");
+        const headerData = await headerResponse.text();
+        document.querySelector("#container").insertAdjacentHTML('afterbegin', headerData);
+
+        const footerResponse = await fetch("https://t2biomod2024.github.io/2024/page_parts/footer.html");
+        const footerData = await footerResponse.text();
+        document.querySelector("#container").insertAdjacentHTML('beforeend', footerData);
+
+        const buttonResponse = await fetch("https://t2biomod2024.github.io/2024/page_parts/button.html");
+        const buttonData = await buttonResponse.text();
+        document.querySelector("body").insertAdjacentHTML('beforeend', buttonData);
+    } catch (error) {
+        console.error('Error loading HTML parts:', error);
+    }
 });
 
 
