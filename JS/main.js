875ff2e542a3fa5fc0bd1d9e6a9b67c972e4849b
Ryusei334 <145414234+Ryusei334@users.noreply.github.com>
2024-09-13 23:04:05 +0900

    ""

--- JS/main.js
+++ JS/main.js
@@ -12,6 +12,7 @@ fetch("https://t2biomod2024.github.io/2024/page_parts/button.html")
     .then((response) => response.text())
     .then((data) => document.querySelector("body").insertAdjacentHTML('beforeend', data));
 
+
 // debounce
 function debounce(func, wait) {
     var timeout;
