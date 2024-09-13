5c716848dcf3d4c8bd71f1cf010cc6eb59db5e9f
Ryusei334 <145414234+Ryusei334@users.noreply.github.com>
2024-09-14 02:53:04 +0900

    comment

--- JS/common.js
+++ JS/common.js
@@ -1,6 +1,8 @@
-// commonlize
-
+//commonlize
 //非同期型の上のコードの方がレスポンスが早いが，読み込み前に他のコードが進行してしまい，表示がバグる．共通化をやめれば早くなるが面倒．最後にそれぞれに書き出すかも．
+
+
+//非同期型
 //fetch("https://t2biomod2024.github.io/2024/page_parts/header.html")
 //    .then((response) => response.text())
 //    .then((data) => document.querySelector("#container").insertAdjacentHTML('afterbegin', data));
@@ -11,6 +13,8 @@
 //    .then((response) => response.text())
 //    .then((data) => document.querySelector("#container").insertAdjacentHTML('afterend', data));
 
+
+//同期型
 function fetchAndInsertSync(url, position) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, false); // 第3引数にfalseを設定すると同期リクエストになります
