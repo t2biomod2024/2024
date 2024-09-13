3ade44f7dc9eaaf323a7ed0cea4ad8c0fe7af4b9
Ryusei334 <145414234+Ryusei334@users.noreply.github.com>
2024-09-14 01:39:57 +0900

    UU

--- JS/common.js
+++ JS/common.js
@@ -1,10 +1,10 @@
 // commonlize
-fetch("https://t2biomod2024.github.io/2024/page_parts/button.html")
-    .then((response) => response.text())
-    .then((data) => document.querySelector("#container").insertAdjacentHTML('afterend', data));
 fetch("https://t2biomod2024.github.io/2024/page_parts/header.html")
     .then((response) => response.text())
     .then((data) => document.querySelector("#container").insertAdjacentHTML('afterbegin', data));
 fetch("https://t2biomod2024.github.io/2024/page_parts/footer.html")
     .then((response) => response.text())
     .then((data) => document.querySelector("#container").insertAdjacentHTML('beforeend', data));
+fetch("https://t2biomod2024.github.io/2024/page_parts/button.html")
+    .then((response) => response.text())
+    .then((data) => document.querySelector("#container").insertAdjacentHTML('afterend', data));
