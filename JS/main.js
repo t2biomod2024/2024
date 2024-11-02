5c679701b72c1ca3484da0e8db3caec87fa6ec6a
Ryusei334 <145414234+Ryusei334@users.noreply.github.com>
2024-11-03 00:25:45 +0900

    ii

--- JS/main.js
+++ JS/main.js
@@ -93,7 +93,7 @@ window.addEventListener("scroll", () => {
     const header = document.querySelector("header");
     const scrollY = window.scrollY;
     const opacity = Math.min(scrollY / 300, 1);
-    header.style.backgroundColor = `rgba(255, 255, 255, ${opacity})`;
+    header.style.backgroundColor = `rgba(240, 240, 240, ${opacity})`;
 });
 
 
