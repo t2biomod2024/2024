9ff00e13c462cea92501dca857a75d0901e24e20
Ryusei334 <145414234+Ryusei334@users.noreply.github.com>
2024-11-10 15:16:40 +0900

    js2

--- JS/main.js
+++ JS/main.js
@@ -248,22 +248,22 @@ function start () {
   
 }
 
-//Toggle "effect" classes
+// Toggle "effect" classes
 function toggleEffect () {
-  var target = document.querySelector (this.dataset.target);
-      target.dataset.effect = this.dataset.effect;
-  
-  for (var i= 0; i < demoButtons.length; i++) {
-    demoButtons[i].classList.remove ('active');
+    var target = document.querySelector (this.dataset.target);
+        target.dataset.effect = this.dataset.effect;
+    
+    for (var i= 0; i < demoButtons.length; i++) {
+      demoButtons[i].classList.remove ('active');
+    }
+    
+    toggleActive.call (this);
   }
   
-  toggleActive.call (this);
-}
-
-//Toggle "active" class
-function toggleActive () {
-  this.classList.toggle ('active');
-}
-
-// Invoke "start ()" function
-window.addEventListener ('load', start);
+  // Toggle "active" class
+  function toggleActive () {
+    this.classList.toggle ('active');
+  }
+  
+  // Invoke "start ()" function
+  window.addEventListener ('load', start);
